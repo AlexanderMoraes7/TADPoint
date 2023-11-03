314f29d11ccb5e4abeff1adde5557f3abc159355
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-03 20:56:04 -0300

    Create TADPoint.java

---
+++ TADPoint.java
@@ -0,0 +1,33 @@
+public class TADPonto{
+  private int coordX;
+  private int coordY;
+
+  public TADPonto(){
+    this.coordX = 0;
+    this.coordY = 0;
+  }
+
+  public TADPonto(int coordX, int coordY){
+    this.coordX = coordX;
+    this.coordY = coordY;
+  }
+
+  public int getCoordX(){
+    return this.coordX;
+  }
+  
+  public int getCoordY(){
+    return this.coordY;
+  }
+
+  public void setCoordX(int coordX){
+    this.coordX = coordX;
+  }
+
+  public void setCoordY(int coordY){
+
+    if ((coordY > 0) && (coordY < 600)){
+      this.coordY = coordY;
+    }
+  }
+}
