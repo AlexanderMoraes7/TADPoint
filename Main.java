f3a6651026d40db3ad04fa13aeef9a5e51d4a543
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-03 20:55:30 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    double x = 10;
+
+    int[] y = new int[10];
+
+    TADPonto p = new TADPonto();
+    p.getCoordX();
+    p.setCoordY(2000);
+    
+    System.out.println(p.getCoordY());
+  }
+}
